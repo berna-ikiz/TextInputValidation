d4753e24f6e29e6e0ebc63e3b43649f365322eac
Berna İkiz <34381430+berna-ikiz@users.noreply.github.com>
2025-04-28 23:47:53 +0300

    Error message has been changed

--- App.tsx
+++ App.tsx
@@ -1,56 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
-import { IbanInputColors } from './constants/colors';
-import { useState } from 'react';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native";
+import { IbanInputColors } from "./constants/colors";
+import { useState } from "react";
 
 export default function App() {
-  const [iban,setIban] = useState('');
-  const [error,setError] = useState('')
-  const [errorVisible, setErrorVisible] = useState(false)
+  const [iban, setIban] = useState("");
+  const [error, setError] = useState("");
+  const [errorVisible, setErrorVisible] = useState(false);
 
-
-  const handleChange = (text:string) => {
-    setErrorVisible(false)
+  const handleChange = (text: string) => {
+    setErrorVisible(false);
     const value = text.trim();
-    let validValue = 'TR'
-    let formattedValue = ''
+    let validValue = "TR";
+    let formattedValue = "";
 
-    if(value.length<29){
-      setError('The length of IBAN could be 26')
-      setErrorVisible(true)
+    if (value.length < 29) {
+      setError("The IBAN must consist of exactly 20 characters.");
+      setErrorVisible(true);
     }
-    
+
     for (let i = 0; i < value.length; i++) {
       const char = value[i];
       if (char.match(/[0-9]/i)) {
-        validValue += char;  
+        validValue += char;
       }
     }
     for (let i = 0; i < validValue.length; i++) {
       if (i > 0 && i % 4 === 0) {
-        formattedValue += ' ';  
+        formattedValue += " ";
       }
-      formattedValue += validValue[i];  
+      formattedValue += validValue[i];
     }
-    formattedValue = formattedValue.trim()
+    formattedValue = formattedValue.trim();
     setIban(formattedValue);
-  
   };
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
-      <TextInput
+        <TextInput
           editable
           numberOfLines={1}
-          onChangeText={text => handleChange(text)}
+          onChangeText={(text) => handleChange(text)}
           style={styles.textInput}
-          placeholder='TR __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __'
+          placeholder="TR __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __"
           maxLength={29}
           value={iban}
           keyboardType="numeric"
         />
         {errorVisible && <Text style={styles.errorText}>{error}</Text>}
-        </View>
+      </View>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -60,25 +58,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: IbanInputColors.container.background,
-    marginTop:40,
-    justifyContent:'center',
+    marginTop: 40,
+    justifyContent: "center",
   },
-  inputContainer:{
-   padding:10,
-   margin:5,
-   gap:2
+  inputContainer: {
+    padding: 10,
+    margin: 5,
+    gap: 2,
   },
-  textInput:{
-    borderWidth:1,
-    borderRadius:2,
-    borderColor:IbanInputColors.normal.border,
-    fontSize:18,
-    backgroundColor:IbanInputColors.normal.background,
+  textInput: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: IbanInputColors.normal.border,
+    fontSize: 18,
+    backgroundColor: IbanInputColors.normal.background,
   },
-  errorText:{
-    color:IbanInputColors.error.text,
-    backgroundColor:IbanInputColors.error.background,
-    padding:5,
-    fontSize:14
-  }
+  errorText: {
+    color: IbanInputColors.error.text,
+    backgroundColor: IbanInputColors.error.background,
+    padding: 5,
+    fontSize: 14,
+  },
 });
